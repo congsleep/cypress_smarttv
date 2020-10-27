f7d7acffb12b82af606552c4875d2bce83d3aae9
congsleep <congnd24@gmail.com>
2020-10-27 23:52:55 +0700

    update case C34

--- cypress/integration/main.js
+++ cypress/integration/main.js
@@ -1,9 +1,32 @@
 import '../function/Setup.js'
+import '../function/On_Error.js'
 
 describe('Login', function (){
+    // Nhập đúng số điện thoại và mã OTP
     it('C34', function (){
-        cy.Homepage();
-        cy.get('.focused > .article > .article-thumb', {timeout: 15000}).should('be.visible')
-        cy.get('.focused > .article > .article-thumb').click();
-    })
+        cy.Homepage()
+        // Kiểm tra hiển thị focus
+        cy.get('.sc-AxiKw > .focused', {timeout: 15000}).should('be.visible')
+        // Truy cập menu Đăng nhập
+        cy.nav_to_menu_login()
+        // Kiểm tra text Header
+        cy.get('.sign-out-name')
+            .contains('Hãy đăng nhập VieON')
+        cy.get('.sign-out-name')
+            .contains('để có những trải nghiệm tốt nhất')
+        // Bấm button Đăng nhập
+        cy.get('.w-11')
+            .trigger('keydown', {keyCode:13, which:13})
+        // Chuyển qua tab đăng nhập bằng SĐT
+        cy.get('.sc-AxjAm > .focused')
+            .trigger('keydown', {keyCode:39, which:39})
+            .trigger('keydown', {keyCode:39, which:39})
+            .trigger('keydown', {keyCode:13, which:13})
+        cy.get('.keyboard', {timeout: 5000}).should('be.visible')
+            .trigger('keydown', {keyCode:40, which:40})
+        cy.nhap_sdt()
+        cy.get('.btn').trigger('keydown', {keyCode:40, which:40}).trigger('keydown', {keyCode:13, which:13})
+        cy.nhap_otp()
+        cy.get('.w-11').trigger('keydown', {keyCode:40, which:40}).trigger('keydown', {keyCode:40, which:40}).trigger('keydown', {keyCode:13, which:13})
+        })
 })
